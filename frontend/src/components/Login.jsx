e09c3c90188fa39d3a1ceff88588bffa14a5853e
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-05-28 03:32:10 +0900

    fix

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,32 @@ import React from "react";
 const Login = ({ setSuccess }) => {
     return (
         <div className="loginDiv">
-            <form className="form" onSubmit={() => setSuccess(true)}>
+            <form
+                className="form"
+                onSubmit={async (e) => {
+                    // e.preventDefault();
+                    // console.log("e: ", e.target);
+                    // const username = e.target.username.value;
+                    // console.log("username: ", username);
+                    // const password = e.target.password.value;
+                    // console.log("password: ", password);
+                    // const formData = new FormData(e.target);
+                    // // console.log("formData: ", formData);
+                    // // const data = Object.fromEntries(formData.entries());
+                    // // console.log(data);
+                    // const response = await fetch("/login", {
+                    //     method: "POST",
+                    //     body: formData,
+                    // });
+
+                    // if (response.ok) {
+                    //     // ログイン成功時の処理
+                    //     setSuccess(true);
+                    // }
+                    setSuccess(true);
+                }}
+            >
+                {/* <form className="form" action="/login" method="post"> */}
                 <div className="inputDiv">
                     <label>ユーザーID </label>
                     <input className="textbox" type="text" name="username" />
