ae560f0b4213b08b9799b1222587efbf392e913f
TMC-MASARU-MATSUDA <masaru_matsuda_ab@mail.toyota.co.jp>
2025-05-01 22:35:33 +0900

    oldbackendを削除

--- oldbackend/src/db/index.js
+++
@@ -1,7 +0,0 @@
-const knex = require("knex");
-// configをknexに統合
-const knexConfig = require("./knexfile");
-// knexfileの環境設定をどちらにするか決定
-const environment = process.env.DATABASE_URL ? "production" : "development";
-
-module.exports = knex(knexConfig[environment]);
--- oldbackend/src/server/index.js
+++
@@ -1,9 +0,0 @@
-const { setupExpressApp } = require("./server");
-
-const server = setupExpressApp();
-// rendar用のポート番号追記
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log("Server listening on Port", PORT);
-});
