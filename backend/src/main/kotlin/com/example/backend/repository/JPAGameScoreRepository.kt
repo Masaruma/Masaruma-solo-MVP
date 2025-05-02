63bed4630b6b584207c50385b826491dd5cb45ac
TMC-MASARU-MATSUDA <masaru_matsuda_ab@mail.toyota.co.jp>
2025-05-02 21:35:43 +0900

    actionsファイルを修正

--- backend/src/main/kotlin/com/example/backend/repository/JPAGameScoreRepository.kt
+++ backend/src/main/kotlin/com/example/backend/repository/JPAGameScoreRepository.kt
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface JPAGameScoreRepository:JpaRepository<Score, Int>{
     // GameMode の gameName で検索
-    fun findByGameModeGameName(gameName: String): List<Score>
     fun findByGameModeGameNameAndGameLevelLevel(gameName: String, level: Int): List<Score>
 }
